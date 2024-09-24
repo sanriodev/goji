62aa243b0e7091c8f90f9df877dcfc30239bbc92
Matteo Juen <matteo.juen@ematric.com>
2024-09-24 13:36:52 +0200

    feat: wip - arrow key selection

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module gomoji
 go 1.23.1
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 
+	"github.com/eiannone/keyboard"
 	"github.com/spf13/cobra"
 )
 
@@ -36,42 +34,78 @@ func main() {
 }
 
 func createEmoji() {
-	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Use arrow keys to select and press Enter to confirm.")
 
-	fmt.Println("Pick a left arm:")
-	leftArm := pickPart(reader, leftArms)
+	// Open the keyboard for capturing inputs
+	if err := keyboard.Open(); err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer keyboard.Close()
 
-	fmt.Println("Pick a left eye:")
-	leftEye := pickPart(reader, eyes)
+	leftArm := pickPart(leftArms, "Pick a left arm:")
 
-	fmt.Println("Pick a mouth:")
-	mouth := pickPart(reader, mouths)
+	leftEye := pickPart(eyes, "Pick a left eye:")
 
-	fmt.Println("Pick a right eye:")
-	rightEye := pickPart(reader, eyes)
+	mouth := pickPart(mouths, "Pick a mouth:")
 
-	fmt.Println("Pick a right arm:")
-	rightArm := pickPart(reader, rightArms)
+	rightEye := pickPart(eyes, "Pick a right eye:")
 
-	// Generate and display the final emoji
-	emoji := fmt.Sprintf("%s%s%s%s%s%s%s", leftArm, "(", leftEye, mouth, rightEye, ")", rightArm)
+	rightArm := pickPart(rightArms, "Pick a right arm:")
+
+	emoji := fmt.Sprintf("%s %s %s %s %s %s %s", leftArm, "(", leftEye, mouth, rightEye, ")", rightArm)
 	fmt.Println("Your custom emoji:", emoji)
 }
 
-func pickPart(reader *bufio.Reader, options []string) string {
+func pickPart(options []string, message string) string {
+	selectedIndex := 0
+
+	for {
+		fmt.Println(message)
+		// Display options with highlighting for the current selection
+		displayOptions(options, selectedIndex)
+
+		// Read the keyboard input
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			fmt.Println("Error reading key and char:", err, char)
+			break
+		}
+
+		// Handle key input
+		switch key {
+		case keyboard.KeyArrowUp:
+			// Move up in the selection list
+			if selectedIndex > 0 {
+				selectedIndex--
+			}
+		case keyboard.KeyArrowDown:
+			// Move down in the selection list
+			if selectedIndex < len(options)-1 {
+				selectedIndex++
+			}
+		case keyboard.KeyEnter:
+			// Confirm selection with Enter
+			fmt.Print("\033[H\033[2J")
+			return options[selectedIndex]
+		case keyboard.KeyEsc:
+			// Exit on ESC (optional)
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		}
+					
+					fmt.Print("\033[H\033[2J")
+	}
+	return options[selectedIndex]
+}
+
+func displayOptions(options []string, selectedIndex int) {
 	for i, option := range options {
-		fmt.Printf("%d: %s\n", i+1, option)
+		if i == selectedIndex {
+			// Highlight the selected option
+			fmt.Printf("> %s\n", option)
+		} else {
+			fmt.Printf("  %s\n", option)
+		}
 	}
-
-	fmt.Print("Enter the number for your choice: ")
-	choiceStr, _ := reader.ReadString('\n')
-	choiceStr = strings.TrimSpace(choiceStr) // Remove any trailing newlines or spaces
-
-	choiceIndex, err := strconv.Atoi(choiceStr) // Convert the string to an integer
-	if err != nil || choiceIndex < 1 || choiceIndex > len(options) {
-		fmt.Println("Invalid choice, please try again.")
-		return pickPart(reader, options)
-	}
-
-	return options[choiceIndex-1]
 }
