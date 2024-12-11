08b7577edda4610482b66818c5fd0dd3259ac10c
Matteo <45847997+sanriodev@users.noreply.github.com>
2024-12-11 14:56:09 +0100

    feat: new flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,41 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/spf13/cobra"
+    "fmt"
+    "os"
+    "github.com/spf13/cobra"
 )
 
+var newFlag bool
+var randomFlag bool
+var favoritesFlag bool
+
 var rootCmd = &cobra.Command{
-	Use:   "goji",
-	Short: "Create custom or random text emojis",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("\033[H\033[2J")
-		ShowMainMenu()
-	},
+    Use:   "goji",
+    Short: "Create custom or random text emojis",
+    Run: func(cmd *cobra.Command, args []string) {
+        fmt.Print("\033[H\033[2J")
+        if newFlag {
+            emoji.CreateCustomEmoji()
+        } else if randomFlag {
+            emoji.CreateRandomEmoji()
+        } else if favoritesFlag {
+            PickFavorite()
+        } else {
+            ShowMainMenu()
+        }
+    },
+}
+
+func init() {
+    rootCmd.Flags().BoolVarP(&newFlag, "new", "n", false, "Create a new emoji")
+    rootCmd.Flags().BoolVarP(&randomFlag, "random", "r", false, "Create a random emoji")
+    rootCmd.Flags().BoolVarP(&favoritesFlag, "favorites", "f", false, "Pick from favorites")
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 }
